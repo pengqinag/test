851858ed19050c778f31775fdef44f3d0bd40514
pengqiang <pengqiang@blocksec.com>
2025-05-12 18:50:32 +0800

    init

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 def sayHello():
     print("Hello, World!")
 
-sayHello()
 
 def sayGoodbye():
     print("Goodbye, World!")
 
+
+sayHello()
+a = 5
+sayGoodbye()
+b = 10
+print("This is a test script.")
+print(a+b)
